0e8cf470ff6e967cf98acd5e32622a125bc66bc2
r0hankrishnan <125333525+r0hankrishnan@users.noreply.github.com>
2026-06-24 20:08:28 -0400

    Update clean_raw_data.py
    Add auto-dated naming for cleaned data + fix package name

--- scripts/clean_raw_data.py
+++ scripts/clean_raw_data.py
@@ -1,15 +1,19 @@
 from pathlib import Path
+from datetime import date
+
 import pandas as pd
 
-from racquet_sem_search.clean import clean_raw_data
+from frame_finder.clean import clean_raw_data
 
 if __name__ == "__main__":
     
     PROJECT_ROOT = Path(__file__).resolve().parents[1] # Get project root based on file location NOT where script is run from 
     DATA_DIR = PROJECT_ROOT / "data"
-    RAW_DATA_PATH = DATA_DIR / "raw" / "racquets_raw_2026_05_16.csv"
+    RAW_DATA_PATH = DATA_DIR / "raw" / "racquets_raw_2026_06_24.csv"
     
-    CLEANED_DATA_PATH = DATA_DIR / "interim" / "racquets_cleaned_2026_05_16.csv"
+    today_date = date.today().strftime("%Y_%m_%d") # Format so it can be sorted
+    
+    CLEANED_DATA_PATH = DATA_DIR / "interim" / f"racquets_cleaned_{today_date}.csv"
     
     CLEANED_DATA_PATH.parent.mkdir(parents = True, exist_ok = True) # Make sure dirs exist
 
