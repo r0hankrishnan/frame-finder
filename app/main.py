2ca82037c1d21a6b3ba63d3dede1e7814ee4cdae
r0hankrishnan <125333525+r0hankrishnan@users.noreply.github.com>
2026-07-03 03:19:43 -0400

    Update main.py
    Get rid of old comments

--- app/main.py
+++ app/main.py
@@ -34,8 +34,8 @@ app = FastAPI(lifespan=lifespan)
 app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler) #type:ignore
 
-app.include_router(search.router) #-> add this later
+app.include_router(search.router) 
 app.include_router(health.router)
-app.include_router(feedback.router) #-> add this later
+app.include_router(feedback.router) 
 
 app.mount("/", StaticFiles(directory="frontend", html=True), name="static")
